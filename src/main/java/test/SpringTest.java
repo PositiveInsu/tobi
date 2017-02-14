65a2b525a500f9eaa023c283a559a1b6a4cf16e6
insu <positiveinsu@gmail.com>
2017-02-14 11:27:01 -0600

    Spring Demo 1

---
+++ src/main/java/test/SpringTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Joinsu on 2017-02-14.
+ */
+public class SpringTest {
+    public static void main(String[] args) {
+        System.out.println( "test");
+    }
+}
